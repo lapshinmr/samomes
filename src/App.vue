137db39cb6afe4d750bf8e68beda7a388877d384
Mikhail Lapshin <lapshinmr@gmail.com>
2020-01-29 14:23:03 +0400

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,69 @@
 <template>
-  <div>
-    <Calculator />
-  </div>
+  <v-app>
+    <v-navigation-drawer app
+      v-model="drawer"
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app
+      color="primary"
+      dark
+      dense
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+          class="fill-height"
+          fluid
+        >
+        <v-row>
+          <v-col>
+            <HelloWorld/>
+            <Calculator />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Calculator from './components/Calculator.vue'
+import HelloWorld from './components/HelloWorld'
+import Calculator from './components/Calculator'
 
 export default {
-  name: 'calculator',
+  name: 'App',
+
   components: {
-    Calculator
-  }
+    HelloWorld, Calculator
+  },
+
+  data: () => ({
+    drawer: null
+  })
 }
 </script>
 
 <style lang="sass">
-body
-  font-family: Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
 </style>
