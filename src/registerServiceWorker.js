0c8bce36f519acebafa56d2f7a2f6d421c0c746d
Mikhail Lapshin <lapshinmr@gmail.com>
2020-03-26 16:19:31 +0400

    change files

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,6 +3,7 @@
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('+')
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
