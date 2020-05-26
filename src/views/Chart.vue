1f925570c7f9f2d99e7b27297d8c96ee4b75f390
Mikhail Lapshin <lapshinmr@gmail.com>
2020-05-26 17:15:25 +0400

    add Chart file

---
+++ src/views/Chart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  props: ['chartData', 'options', 'styles'],
+  mounted () {
+    this.renderChart(this.chartData, {
+      maintainAspectRatio: false,
+      responsive: true
+    })
+  },
+  watch: {
+    chartData () {
+      this.renderChart(this.chartData, {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
