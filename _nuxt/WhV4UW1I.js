8225f994e54dd267d18305127f9134cea12ef722
Mikhail Lapshin <lapshinmr@gmail.com>
2025-06-27 15:26:37 +0400

    Updates

---
+++ _nuxt/WhV4UW1I.js
@@ -0,0 +1,1 @@
+import{a2 as t,f as e,F as s,bw as n}from"./CpVNxatc.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
